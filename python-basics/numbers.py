4ccc3e52675e7cda4ddf0ed8c5f66a5baa1fbcb4
Abdulrahman AL-Janabi <abdulrahmanal-janabi@Abdulrahmans-MacBook-Pro.local>
2026-01-15 00:28:32 +0300

    started with data types and now on variables

---
+++ python-basics/numbers.py
@@ -0,0 +1,16 @@
+# int & float
+# float gets more space in memory than int, since it stores two different locations one before floating point and one after it.
+
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+print(type(0.0001))
+print(type(0))
+print(type(20+1.1))
+print(9.9+1.1)
+
+print(2 ** 2) # 2 ^ 2 = 4
+print(2 // 4) # rounded down into nearest integer
+print(6 % 4) # modula, reminder of 4
