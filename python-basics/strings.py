34dd59115dd8ef4435926c7d78146af27c51e6e4
Abdulrahman AL-Janabi <abdulrahmanal-janabi@Abdulrahmans-MacBook-Pro.local>
2026-01-15 23:51:02 +0300

    continue on variables

---
+++ python-basics/strings.py
@@ -0,0 +1,27 @@
+print(type("ho hello there 2"))
+
+username = 'supercoder'
+password = 'supersecret'
+long_string = '''
+WOW
+O O
+---
+'''
+
+print(long_string)
+
+first_name = 'AR'
+last_name = 'Janabi'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+# string concatenation (only works with strings)
+print('hello ' + 'AR')
+
+# type conversion
+print(type(int(str(100))))
+
+# escape sequence
+# it solves once we put a backslash before the value
+weather = "\tIt\'s \"kind of\" sunny\nhope you have a good day"
+print(weather)
