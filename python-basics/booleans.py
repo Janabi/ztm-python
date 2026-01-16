72e8cc93f1dc54b56728a71f737e122869fa323d
Abdulrahman AL-Janabi <abdulrahmanal-janabi@Abdulrahmans-MacBook-Pro.local>
2026-01-16 23:32:40 +0300

    trying some exercises and string, booleans, and built in methods

---
+++ python-basics/booleans.py
@@ -0,0 +1,8 @@
+# booleans
+name = 'AR Janabi'
+is_cool = False
+
+is_cool = True
+
+print(bool(1))
+print(bool(0))
---
+++ python-basics/builtinMethods.py
@@ -0,0 +1,13 @@
+# for string methods: https://www.w3schools.com/python/python_ref_string.asp
+# https://docs.python.org/3/library/functions.html
+# for comments: https://realpython.com/python-comments-guide/
+greet = 'Helllooooo'
+print(len(greet))
+
+quote = 'to be or not to be'
+
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+print(quote)
