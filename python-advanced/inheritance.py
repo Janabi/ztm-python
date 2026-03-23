90da09a57799865acb58b20c07d7058d8fe4331b
AR Janabi Personal Ubuntu <aaljanabi1992@gmail.com>
2026-03-24 00:34:43 +0300

    continue with the basic of oop, inheritance, abstraction, polymorphism, and encapsulation, dunder methods, and introspection

--- python-advanced/inheritance.py
+++ python-advanced/inheritance.py
@@ -1,6 +1,9 @@
 class User:
     def sign_in(self):
         print('logged in')
+    
+    def attack(self):
+        print('do nothing')
 
 class Wizard(User):
     def __init__(self, name, power):
@@ -8,6 +11,7 @@ class Wizard(User):
         self.power = power
 
     def attack(self):
+        User.attack(self)
         print(f'attacking with power of {self.power}')
 
 class Archer(User):
@@ -21,5 +25,19 @@ class Archer(User):
     def run(self):
         print('ran really fast')
 
+    def attack(self):
+        print(f'attacking with arrows: arrows left {self.num_arrows}')
+
 wizard1 = Wizard('Merlin', 50)
 print(wizard1.sign_in())
+print(wizard1.attack())
+
+print(isinstance(wizard1, User))
+print(isinstance(wizard1, Wizard))
+print(isinstance(wizard1, Archer))
+print(isinstance(wizard1, object))
+
+archer1 = Archer('Robin', 100)
+print(archer1.sign_in())
+print(archer1.check_arrows())
+print(archer1.run())
---
+++ python-advanced/super.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self, email):
+        self.email = email
+
+    def sign_in(self):
+        print('logged in')
+
+class Wizard(User):
+    def __init__(self, name, power, email):
+        # User.__init__(self, email)
+        super().__init__(email)
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'attacking with power of {self.power}')
+
+wizard1 = Wizard('Merlin', 50, 'merlin@gmail.com')
+print(wizard1.sign_in())
+print(wizard1.attack())
+print(dir(wizard1)) # introspection
