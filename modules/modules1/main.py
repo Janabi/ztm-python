f6dbb391722f06b5bf2939ead488b6f8dc84175a
AR Janabi Personal Ubuntu <aaljanabi1992@gmail.com>
2026-03-31 00:33:14 +0300

    continue with custom packages and modules, and how we can import them

---
+++ modules/modules1/main.py
@@ -0,0 +1,10 @@
+# import utility
+from utility import multiply, divide, add, subtract
+# import shopping.shopping_cart
+from shopping.shopping_cart import buy
+
+print(multiply(10, 20))
+print(divide(10, 20))
+print(add(10, 20))
+print(subtract(10, 20))
+print(buy('apple'))
